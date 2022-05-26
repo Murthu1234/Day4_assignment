88cc0bc6012cb25862c8841b0ea5492e26b86d80
Murthu1234 <murthuja1106@gmail.com>
2022-05-26 22:41:53 +0530

    Day4-assignments

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -1,2 +1,15 @@
-package TicTacToe;public class TicTacToeGame {
+package TicTacToe;
+
+public class TicTacToeGame {
+    void TicTacToeMethod(){
+        char[] board;
+    }
+
+    public static void main(String[] args) {
+        char board;
+        System.out.println("welcome to TicTacToe class");
+
+
+
+    }
 }
